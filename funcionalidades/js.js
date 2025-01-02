371bf5c00e89a7def4935c79875151056b44f287
Rubén Jofré <132699433+rukkysy@users.noreply.github.com>
2025-01-02 20:37:47 -0300

    Add files via upload

---
+++ funcionalidades/js.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.project-card1').forEach(function(div) {
+     div.addEventListener('click', function() {
+       window.location.href = 'https://rukkysy.github.io/VolcandeDulzura/';
+     });
+   });
+
+   document.querySelectorAll('.project-card2').forEach(function(div) {
+     div.addEventListener('click', function() {
+       window.location.href = 'https://rukkysy.github.io/celialamexicana/';
+     });
+   });
+
+   document.querySelectorAll('.project-card3').forEach(function(div) {
+     div.addEventListener('click', function() {
+       window.location.href = 'https://rukkysy.github.io/fanatiko/';
+     });
+   });
